ed7ce28f6092463e96271edef045d08843aea693
Rahul Rajaram <rahulrajaram2005@gmail.com>
2018-06-03 00:20:34 -0700

    Add ability to filter by header file; text used to filter output will be colored

--- dependency_mapper/dependency_mapper.py
+++ dependency_mapper/dependency_mapper.py
@@ -4,7 +4,8 @@ import sys
 import argparse
 
 import six
-from tabulate import tabulate
+import tabulate
+import termcolor
 
 
 class FormatterClass(argparse.RawDescriptionHelpFormatter):
@@ -19,11 +20,19 @@ def grep_output():
     return string_output.strip().split('\n')
 
 
-def header_dict():
+def header_dict(args_context):
     _header_dict = dict()
     for line in grep_output():
         file_name, header_name = [component.strip().strip('"') for component in line.split(':#include')]
         if not file_name or not header_name:
+            continue
+        if (
+            args_context.highlight
+            and not (
+                [f for f in args_context.highlight if file_name in f or f in file_name]
+                or [f for f in args_context.highlight if header_name in f or f in header_name]
+            )
+        ):
             continue
 
         if _header_dict.get(file_name):
@@ -33,16 +42,16 @@ def header_dict():
     return _header_dict
 
 
-def header_list():
+def header_list(args_context):
     _file_to_headers_mappings_list = list()
-    for key, value in six.iteritems(header_dict()):
+    for key, value in six.iteritems(header_dict(args_context)):
         _file_to_headers_mappings_list.append([key, '\n'.join(value)])
     _file_to_headers_mappings_list.sort(key=lambda mapping: mapping[0])
 
     return _file_to_headers_mappings_list
 
 
-def help_text_interception():
+def arguments_context():
     parser = argparse.ArgumentParser(
     description="""Simple tool to draw the header-file dependency map of your C/C++ project.',
 
@@ -52,10 +61,27 @@ def help_text_interception():
 """,
         formatter_class=FormatterClass
     )
-    parser.parse_args()
+    parser.add_argument(
+        '-hl',
+        '--highlight',
+        default=[],
+        help='specify header file names to highlight',
+        nargs='*'
+    )
+
+    return parser.parse_args()
+
+
+def output(args_context):
+    output = tabulate.tabulate(header_list(args_context), tablefmt='grid')
+
+    for header in args_context.highlight:
+        colored_version = termcolor.colored(header, 'red', attrs=['bold', 'underline'])
+        print(colored_version)
+        output = output.replace(header, colored_version)
+
+    print(output)
 
 
 def describe_headers():
-    help_text_interception()    
-
-    print(tabulate(header_list(), tablefmt='grid'))
+    output(arguments_context())
